e0c478388e8b41e41949d3150e6db5ed47ecbe24
Volodymyr-Tuman <volodiap467@gmail.com>
2025-05-20 16:36:55 +0200

    Update kartki.js

--- style/JS/kartki.js
+++ style/JS/kartki.js
@@ -1,28 +1,29 @@
-  const ukrainianSentences = [
-  "Я замовив піцу на вечерю", "Вона читає цікаву статтю про науку", "Ми грали в футбол після школи", "Вони дивилися фільм учора ввечері", "Я написав листа бабусі",
-  "Моя сестра пішла на йогу", "Ми слухали музику в парку", "Ти вже зробив домашнє завдання?", "Вони танцювали всю ніч", "Я навчився готувати борщ",
-  "Ми збираємось у похід цієї суботи", "Він купив новий смартфон", "Я побачив веселку після дощу", "Ми прибирали квартиру вранці", "Я зробив фотографію заходу сонця",
-  "Вона пішла в бібліотеку за книжкою", "Ми відвідали нову виставку в галереї", "Я замовив каву без цукру", "Вони каталися на ковзанах у парку", "Я розповів жарт друзям",
-  "Ми грали на гітарі біля вогнища", "Я прокинувся дуже рано", "Ми приготували смачну вечерю", "Ти бачив мій рюкзак?", "Я вивчаю німецьку мову",
-  "Моя мама дивиться кулінарне шоу", "Ми плануємо вечірку на вихідні", "Я отримав листа від друга з-за кордону", "Ми гуляли на березі річки", "Вона малювала портрет кота",
-  "Я скачав нову гру на телефон", "Ми співали пісні в машині", "Він зламав навушники", "Я писав повідомлення на уроці", "Ми годували качок у парку",
-  "Вона запізнилась на зустріч", "Я знайшов ключі під ліжком", "Ми розв'язали складну задачу", "Ти пригадав її ім’я?", "Він лагодив велосипед",
-  "Ми плавали в озері", "Я навчився нової пісні", "Вона розповідала цікаву історію", "Ми побудували хатку на дереві", "Я натиснув не ту кнопку",
-  "Ми заблукали в лісі", "Я намалював картину аквареллю", "Вони прикрашали кімнату на свято", "Ти приготував сніданок?", "Ми обговорювали книгу"
+const ukrainianSentences = [
+  "Я з'їв бутерброд", "Вона подзвонила мамі", "Ми зустрілися ввечері", "Вони купили новий диван", "Я написав повідомлення",
+  "Тато помив машину", "Ми поїхали в супермаркет", "Ти бачив мій телефон?", "Діти грали у дворі", "Я закрив двері",
+  "Ми поїхали на море", "Він забув ключі", "Я включив телевізор", "Ми зварили борщ", "Я зробив фото на памʼять",
+  "Сестра принесла яблука", "Ми слухали музику в машині", "Я купив воду і хліб", "Вони катались на роликах", "Я показав їм дорогу",
+  "Ми грали в шахи", "Я встав рано", "Ми зробили сніданок", "Ти бачив наш дім?", "Я вчу польську",
+  "Мама приготувала вечерю", "Ми організували поїздку", "Я отримав подарунок", "Ми гуляли парком", "Вона малювала квіти",
+  "Я встановив додаток", "Ми співали пісні", "Він зламав пульт", "Я писав у зошиті", "Ми погодували собаку",
+  "Вона прийшла о сьомій", "Я знайшов телефон", "Ми вирішили приклад", "Ти памʼятаєш її імʼя?", "Він полагодив замок",
+  "Ми плавали в озері", "Я вивчив пісню", "Вона розказала історію", "Ми побудували курінь", "Я натиснув кнопку",
+  "Ми заблукали дорогою", "Я намалював машину", "Вони прикрасили кімнату", "Ти зробив чай?", "Ми обговорили план"
 ];
 
 const polishSentences = [
-  "Zamówiłem pizzę na kolację", "Ona czyta ciekawy artykuł o nauce", "Graliśmy w piłkę po szkole", "Oglądali film wczoraj wieczorem", "Napisałem list do babci",
-  "Moja siostra poszła na jogę", "Słuchaliśmy muzyki w parku", "Czy odrobiłeś już pracę domową?", "Tańczyli całą noc", "Nauczyłem się gotować barszcz",
-  "Wybieramy się na wędrówkę w tę sobotę", "Kupił nowy smartfon", "Zobaczyłem tęczę po deszczu", "Sprzątaliśmy mieszkanie rano", "Zrobiłem zdjęcie zachodu słońca",
-  "Poszła do biblioteki po książkę", "Odwiedziliśmy nową wystawę w galerii", "Zamówiłem kawę bez cukru", "Jeździli na łyżwach w parku", "Opowiedziałem dowcip znajomym",
-  "Graliśmy na gitarze przy ognisku", "Obudziłem się bardzo wcześnie", "Przygotowaliśmy pyszną kolację", "Widziałeś mój plecak?", "Uczę się języka niemieckiego",
-  "Moja mama ogląda program kulinarny", "Planujemy imprezę na weekend", "Dostałem list od przyjaciela z zagranicy", "Spacerowaliśmy nad rzeką", "Ona narysowała portret kota",
-  "Pobrałem nową grę na telefon", "Śpiewaliśmy piosenki w samochodzie", "Zepsuł słuchawki", "Pisałem wiadomość na lekcji", "Karmiliśmy kaczki w parku",
-  "Spóźniła się na spotkanie", "Znalazłem klucze pod łóżkiem", "Rozwiązaliśmy trudne zadanie", "Przypomniałeś sobie jej imię?", "Naprawiał rower",
-  "Pływaliśmy w jeziorze", "Nauczyłem się nowej piosenki", "Ona opowiadała ciekawą historię", "Zbudowaliśmy domek na drzewie", "Kliknąłem nie ten przycisk",
-  "Zgubiliśmy się w lesie", "Namalowałem obraz akwarelą", "Ozdabiali pokój na święto", "Przygotowałeś śniadanie?", "Dyskutowaliśmy o książce"
+  "Zjadłem kanapkę", "Ona zadzwoniła do mamy", "Spotkaliśmy się wieczorem", "Kupili nową kanapę", "Napisałem wiadomość",
+  "Tata umył samochód", "Pojechaliśmy do supermarketu", "Widziałeś mój telefon?", "Dzieci bawiły się na podwórku", "Zamknąłem drzwi",
+  "Pojechaliśmy nad morze", "On zapomniał kluczy", "Włączyłem telewizor", "Ugotowaliśmy barszcz", "Zrobiłem zdjęcie na pamiątkę",
+  "Siostra przyniosła jabłka", "Słuchaliśmy muzyki w samochodzie", "Kupiłem wodę i chleb", "Jeździli na rolkach", "Pokazałem im drogę",
+  "Graliśmy w szachy", "Wstałem wcześnie", "Zrobiliśmy śniadanie", "Widziałeś nasz dom?", "Uczę się polskiego",
+  "Mama zrobiła kolację", "Zorganizowaliśmy wycieczkę", "Dostałem prezent", "Spacerowaliśmy po parku", "Ona rysowała kwiaty",
+  "Zainstalowałem aplikację", "Śpiewaliśmy piosenki", "Zepsuł pilot", "Pisałem w zeszycie", "Nakarmiliśmy psa",
+  "Przyszła o siódmej", "Znalazłem telefon", "Rozwiązaliśmy przykład", "Pamiętasz jej imię?", "Naprawił zamek",
+  "Pływaliśmy w jeziorze", "Nauczyłem się piosenki", "Opowiedziała historię", "Zbudowaliśmy szałas", "Nacisnąłem przycisk",
+  "Zgubiliśmy się po drodze", "Narysowałem samochód", "Ozdobili pokój", "Zrobiłeś herbatę?", "Omówiliśmy plan"
 ];
+
 
   let currentIndex = 0;
   const cards = [];
