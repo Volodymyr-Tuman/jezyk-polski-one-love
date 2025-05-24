f57698ab0441a1247f553f5de657ae80d8050a5b
Volodymyr-Tuman <volodiap467@gmail.com>
2025-05-24 21:44:39 +0200

    Update tekst2.js

--- style/JS/tekst2.js
+++ style/JS/tekst2.js
@@ -1,15 +1,16 @@
 const correctAnswers = {
-  q1: "a", // We wrześniu
-  q2: "c", // Pomógł mu uczeń z klasy maturalnej
-  q3: "b", // Pomocna i otwarta
-  q4: "a", // Gra na gitarze
-  q5: "a", // Dołączenie do zespołu muzycznego
-  q6: "b", // Nauczyciele zachęcali do dyskusji
-  q7: "b", // Historia i język polski
-  q8: "b", // W szkolnym bufecie
-  q9: "b", // Pewniej i bardziej zadowolony
-  q10: "a" // Że zmiany są trudne, ale wartościowe
+  q1: "b", // Nawet do 60 tysięcy
+  q2: "c", // Zapładniają królową
+  q3: "c", // Masowe wymieranie pszczół
+  q4: "c", // Dzięki tańcowi pszczelemu
+  q5: "d", // Syrop kukurydziany
+  q6: "a", // Sadzenie roślin miododajnych
+  q7: "c", // Pomagają w komunikacji wewnętrznej
+  q8: "a", // Nie produkują miodu i żyją pojedynczo
+  q9: "c", // Zaburzy się cały łańcuch pokarmowy
+  q10: "b" // Sadzić rośliny przyjazne pszczołom
 };
+
 
     function checkAnswers() {
       for (let key in correctAnswers) {
