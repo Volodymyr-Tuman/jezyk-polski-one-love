ea813df9a4ce35d898b834542ed2cd52b2da2965
Volodymyr-Tuman <volodiap467@gmail.com>
2025-06-22 08:23:41 +0200

    Update tekst2.js

--- style/JS/tekst2.js
+++ style/JS/tekst2.js
@@ -1,14 +1,14 @@
 const correctAnswers = {
-  q1: "b", // Nawet do 60 tysięcy
-  q2: "c", // Zapładniają królową
-  q3: "c", // Masowe wymieranie pszczół
-  q4: "c", // Dzięki tańcowi pszczelemu
-  q5: "d", // Syrop kukurydziany
-  q6: "a", // Sadzenie roślin miododajnych
-  q7: "c", // Pomagają w omunikacji wewnętrznej
-  q8: "a", // Nie produkują miodu i żyją pojedynczo
-  q9: "c", // Zaburzy się cały łańcuch pokarmowy
-  q10: "b" // Sadzić rośliny przyjazne pszczołom
+  q1: "b",
+  q2: "b",
+  q3: "b",
+  q4: "b",
+  q5: "a",
+  q6: "b",
+  q7: "a",
+  q8: "b",
+  q9: "b",
+  q10: "b"
 };
 
 
