d3875be124316a06415c062b05f816d39610a90a
Volodymyr-Tuman <volodiap467@gmail.com>
2025-07-30 16:17:53 +0200

    Update tekst1.js

--- style/JS/tekst1.js
+++ style/JS/tekst1.js
@@ -1,10 +1,16 @@
 const correctAnswers = {
-  q1: "b",
-  q2: "b",
-  q3: "b",
-  q4: "a",
-  q5: "b"
+  q1: "d",
+  q2: "c",
+  q3: "d",
+  q4: "c",
+  q5: "b",
+  q6: "c",
+  q7: "c",
+  q8: "b",
+  q9: "b",
+  q10: "b"
 };
+
 
 
 
